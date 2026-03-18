aca84c08e2ad9235ac33f3b26e711345dbf7971c
Guillermo Gutierrez Almazor <ggalmazor@gmail.com>
2026-03-18 20:21:50 +0100

    Add maven-publish plugin and POM; fix stale Point#getX() Javadoc references

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
   `java-library`
   checkstyle
+  id("com.vanniktech.maven.publish") version "0.36.0"
   id("me.champeau.jmh") version "0.7.3"
 }
 
@@ -29,6 +30,39 @@ tasks.named<Test>("test") {
 
 tasks.javadoc {
   source = sourceSets.main.get().allJava
+}
+
+mavenPublishing {
+  publishToMavenCentral(automaticRelease = true)
+  signAllPublications()
+
+  coordinates("com.ggalmazor", "downsampling", "25.2.0")
+
+  pom {
+    name.set("downsampling")
+    description.set("Java library providing LTTB, RDP, and PIP time-series downsampling algorithms")
+    inceptionYear.set("2025")
+    url.set("https://github.com/ggalmazor/downsampling")
+    licenses {
+      license {
+        name.set("Apache-2.0")
+        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+        distribution.set("repo")
+      }
+    }
+    developers {
+      developer {
+        id.set("ggalmazor")
+        name.set("Guillermo Gutierrez Almazor")
+        url.set("https://github.com/ggalmazor/")
+      }
+    }
+    scm {
+      url.set("https://github.com/ggalmazor/downsampling/")
+      connection.set("scm:git:git://github.com/ggalmazor/downsampling.git")
+      developerConnection.set("scm:git:ssh://git@github.com/ggalmazor/downsampling.git")
+    }
+  }
 }
 
 checkstyle {
